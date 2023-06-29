d4739dcf0b3396dcb0d786244d941b0f5aaabf06
S Kakio <kakikaki154@DESKTOP-69E6BD8.localdomain>
2023-06-29 20:47:55 +0900

    ServiceWorkderを変更

--- GithubPages/wwwroot/service-worker.js
+++ GithubPages/wwwroot/service-worker.js
@@ -2,3 +2,10 @@
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
 self.addEventListener('fetch', () => { });
+
+
+// サービスワーカ-登録済でリクエストを感知した際に発火するイベント
+self.addEventListener('fetch', (event) => {
+	// ブラウザー既定のfetchハンドリングを抑止して、 自前でResponse用のPromiseを提供する
+	console.log(event.request);
+})
