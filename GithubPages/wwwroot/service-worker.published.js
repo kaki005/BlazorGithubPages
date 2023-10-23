52c61ecd09168e2281e0e4998c13b9ee1e743383
kaki005 <83001142+kaki005@users.noreply.github.com>
2023-10-24 01:21:47 +0900

    Update service-worker.published.js

--- GithubPages/wwwroot/service-worker.published.js
+++ GithubPages/wwwroot/service-worker.published.js
@@ -12,7 +12,7 @@ const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, 
 const offlineAssetsExclude = [ /^service-worker\.js$/ ];
 
 async function onInstall(event) {
-    console.info('Service worker: Install');
+    #console.info('Service worker: Install');
 
     // Fetch and cache all matching items from the assets manifest
     const assetsRequests = self.assetsManifest.assets
@@ -20,16 +20,17 @@ async function onInstall(event) {
         .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
         .map(asset => new Request(asset.url, { integrity: asset.hash, cache: 'no-cache' }));
     await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
+    event.waitUntil(self.skipWaiting());        // すぐにactivateにする
 }
 
 async function onActivate(event) {
-    console.info('Service worker: Activate');
 
     // Delete unused caches
     const cacheKeys = await caches.keys();
     await Promise.all(cacheKeys
         .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
         .map(key => caches.delete(key)));
+    event.waitUntil(self.clients.claim());    // すぐにserviceWorkerを有効にする
 }
 
 async function onFetch(event) {
